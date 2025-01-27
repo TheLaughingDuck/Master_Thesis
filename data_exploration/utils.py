434360688a55af1828d146e251dad3b7e5a8f4f3
Simon Jorstedt <jorstedtsimon@gmail.com>
2025-01-27 08:29:40 +0100

    Updated utils

--- data_exploration/utils.py
+++ data_exploration/utils.py
@@ -3,6 +3,13 @@ import pandas as pd
 
 def unique(series, ascending=False):
     '''Returns the unique values, along with the corresponding counts for a series'''
+
+    # If at least one element is str, then the others should be treated as str as well.
+    # The others might be nan for example, which does not play nicely with str.
+    if str in set(type(i) for i in series):
+        series = [str(i) for i in series]
+    
+    # Count frequencies
     counts = np.unique(series, return_counts=True)
     df = pd.DataFrame({"Values": counts[0], "Counts":counts[1]})
     df.sort_values(inplace=True, by="Counts", ascending=ascending)
