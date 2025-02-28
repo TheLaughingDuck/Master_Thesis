43a30c90546495cd65900d98a90fe2b59f410bb5
Simon Jorstedt <jorstedtsimon@gmail.com>
2025-02-28 15:18:27 +0100

    Expanded meta processing script

--- processing/process_meta.py
+++ processing/process_meta.py
@@ -1,6 +1,7 @@
 '''
 This script loads patient meta data from an excel spreadsheet, and performs filtering, renaming etc.
-The result is stored locally for further processing.
+The result is stored locally for further processing. There are a few stepwise saves. The final output
+is pickle file of a pandas dataframe with the observations (with filenames to the volumes).
 '''
 # %%
 # SETUP META PROCESSING
@@ -13,6 +14,15 @@ from utils import *
 
 # Load raw excel data
 data_raw = pd.read_excel("/local/data1/simjo484/mt_data/all_data/MRI/MRI_summary_extended_simon.xlsx")
+
+
+
+# %%
+# META PROCESSING SETTINGS
+
+# Sometimes there are multiple files for the same patient, same session, and same sequence type (like T2W for example).
+# If True, drop all but one
+drop_duplicates = True
 
 
 # %%
@@ -42,8 +52,9 @@ rename = {"Low-grade glioma/astrocytoma (WHO grade I/II)": "Low-Grade Glioma",
 data.loc[:,"diagnosis"] = [rename[diag] for diag in data["diagnosis"]]
 
 
-# Filter out diagnoses
+# Filter out some diagnoses
 data = data[data["diagnosis"] != "Other"]
+
 
 # Combine sequence types
 new_sequences = []
@@ -54,10 +65,10 @@ for seq_type in data["image_type"]:
     if seq_type in ["UNKNOWN", "T1W_FSPGR_GD", "T1W_FSPGR", "SWAN", "SWI", "ASL", "MAG", "PHE", "ANGIO", "Vs3D", "FD", "EXP"]: new_type = "remove"
     
     # These are T1W
-    elif seq_type in ["T1W_SE", "T1W"]: new_type = "T1W"
+    elif seq_type in ["T1W_SE", "T1W", "T1W_MPRAGE", "MPR"]: new_type = "T1W"
 
     # These are T1W-GD
-    elif seq_type in ["T1W_SE_GD", "T1W_SE_GD", "T1W_GD", "T1W-GD"]: new_type = "T1W-GD"
+    elif seq_type in ["T1W_SE_GD", "T1W_SE_GD", "T1W_GD", "T1W-GD", "T1W_MPRAGE_GD"]: new_type = "T1W-GD"
 
     # These are T1W FLAIR
     elif seq_type in ["T1W_FL"]: new_type = "T1W_FLAIR"
@@ -66,10 +77,10 @@ for seq_type in data["image_type"]:
     elif seq_type in ["T1W_FL_GD"]: new_type = "T1W_FLAIR_GD"
 
     # These are MPR (separate from T1)
-    elif seq_type in ["T1W_MPRAGE"]: new_type = "MPR"
+    #elif seq_type in ["T1W_MPRAGE"]: new_type = "MPR"
 
     # These are T1W-MPRAGE_GD
-    elif seq_type in ["T1W_MPRAGE_GD"]: new_type = "T1W_MPRAGE_GD"
+    #elif seq_type in ["T1W_MPRAGE_GD"]: new_type = "T1W_MPRAGE_GD"
     
     # These are T2W
     elif seq_type in ["FSE", "tse", "T2W"]: new_type = "T2W"
@@ -82,6 +93,7 @@ for seq_type in data["image_type"]:
 
     new_sequences.append(new_type)
 data.loc[:, "image_type"] = new_sequences
+
 
 # Filter out unknown image types
 data = data[data["image_type"] != "remove"]
@@ -106,6 +118,8 @@ meta_seq_dummies.loc[:, "found_filename"] = pd.Series([False for i in range(meta
 
 print(f"Meta data shape: {meta_seq_dummies.shape}")
 
+
+
 # %%
 # MATCH FILENAMES
 # Dictionary with the number of files that were, and were *not* matched/found among the files
@@ -115,10 +129,7 @@ non_matches = {seq: 0 for seq in seq_group}
 # Get file names (Final processed)
 file_names = os.listdir("/local/data1/simjo484/mt_data/all_data/MRI/pre_processed/Final preprocessed files/")
 
-
-# %%
 import difflib
-import itertools
 
 printout = False
 
@@ -183,9 +194,85 @@ print("\nTotal: "+str(sum(non_matches.values()) + sum(matches.values())))
 meta_seq_dummies = meta_seq_dummies[meta_seq_dummies["file_found"] == True]
 
 # %%
-# Format and save the meta data locally as "meta.pkl".
+# SAVE META DATA
 ###################################################
 with open("/local/data1/simjo484/mt_data/all_data/MRI/simon/meta.pkl", "wb") as f:
     pickle.dump(meta_seq_dummies, f)
+print("Saved processed meta data to meta.pkl (supposedly)")
+meta = meta_seq_dummies
 
+# %%
+# PIVOT TO GET DATAFRAME WITH OBSERVATIONS
+
+# Drop duplicates
+# See top of this file (META PROCESSING SETTINGS)
+if drop_duplicates:
+    observations = meta.drop_duplicates(subset=["subjetID", "session_name", "seq_type"])
+    observations = observations.pivot(index=["subjetID", "session_name", "diagnosis"], 
+                            values="found_filename",
+                            columns="seq_type")
+    observations = observations.reset_index()
+    observations = observations.fillna("---")
+    print(f"Observations shape after pivoting (and dropping duplicates): {observations.shape}")
+else:
+    meta["type_counter"] = meta.groupby(["subjetID", "session_name", "seq_type", "diagnosis"]).cumcount()+1
+
+    observations = meta.pivot(index=["subjetID", "session_name", "diagnosis", "type_counter"], 
+                            values="found_filename",
+                            columns="seq_type")
+    observations = observations.reset_index()
+    observations = observations.fillna("---")
+    print(f"Observations shape after pivoting (and *not* dropping duplicates): {observations.shape}")
+
+
+
+# %%
+# SAVE OBSERVATION DATA
+###################################################
+with open("/local/data1/simjo484/mt_data/all_data/MRI/simon/observations.pkl", "wb") as f:
+    pickle.dump(observations, f)
+print("Saved observation data to observations.pkl (supposedly)")
+
+
+
+# %%
+# CREATE BINARY COLUMNS FOR SEQ TYPE PAIRS
+observations["T1_and_T1GD"] = (observations["T1W"] != "---") & (observations["T1W-GD"] != "---")
+observations["T1GD_and_T2"] = (observations["T1W-GD"] != "---") & (observations["T2W"] != "---")
+observations["T1_and_T2"] = (observations["T1W"] != "---") & (observations["T2W"] != "---")
+observations["all_three"] = (observations["T1W"] != "---") & (observations["T1W-GD"] != "---") & (observations["T2W"] != "---")
+
+
+
+# %%
+# PRODUCE SOME INFORMATION
+print(f"Total number of rows (observations): {observations.shape[0]}")
+print(f"Total number of unique (patient, session): {observations.drop_duplicates(subset=["subjetID", "session_name"]).shape[0]}")
+print(f"Total number of unique patients: {observations.drop_duplicates(subset=["subjetID"]).shape[0]}")
+print("")
+
+print(f"Number of T1W volumes: {observations[observations["T1W"] != "---"].shape[0]}")
+print(f"Number of T1W-GD volumes: {observations[observations["T1W-GD"] != "---"].shape[0]}")
+print(f"Number of T2W volumes: {observations[observations["T2W"] != "---"].shape[0]}")
+print("")
+
+print(f"Number of patients with T1W, T1W-GD and T2W: {observations[(observations["T1W"] != "---") & (observations["T1W-GD"] != "---") & (observations["T2W"] != "---")].shape}")
+print(f"Number of patients with T1W and T1W-GD: {observations[(observations["T1W"] != "---") & (observations["T1W-GD"] != "---")].shape[0]}")
+print(f"Number of patients with T1W and T2W: {observations[(observations["T1W"] != "---") & (observations["T2W"] != "---")].shape[0]}")
+print(f"Number of patients with T1W-GD and T2W: {observations[(observations["T1W-GD"] != "---") & (observations["T2W"] != "---")].shape[0]}")
+print("")
+
+
+print("\nNumber of observations with 2, or 3 out of (T1, T1GD and T2) for each diagnose:\n")
+print(observations.groupby("diagnosis")[["T1_and_T1GD", "T1GD_and_T2", "T1_and_T2", "all_three"]].sum().reset_index())
+print("\n")
+
+
+
+# %%
+# SAVE FINAL OBSERVATION DATA
+###################################################
+with open("/local/data1/simjo484/mt_data/all_data/MRI/simon/final_observations.pkl", "wb") as f:
+    pickle.dump(observations, f)
+print("Saved observation data to final_observations.pkl (supposedly)")
 # %%
