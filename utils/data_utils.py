abef5238629e7e7065a901d86c4fa3b74e6e510d
Simon Jorstedt <jorstedtsimon@gmail.com>
2025-03-19 09:55:54 +0100

    Improved classifier data loader: Added data augmentation

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -27,6 +27,8 @@ import pickle
 
 import matplotlib.pyplot as plt
 
+import collections
+
 from monai.networks.nets import SwinUNETR
 from monai import data
 from monai.transforms import (
@@ -39,23 +41,49 @@ from monai.transforms import (
     Compose,
     Rotate90d,
     Lambda,
-    ToDeviced
+    ToDeviced,
+    RandRotate90d,
+    RandFlipd
 )
 
 def get_loader(args):
     
-    save_dir = "/local/data2/simjo484/Classifier_training/"
-    with open(save_dir+"t2_training_paths.pkl", "rb") as f:
-        train_data_paths = pickle.load(f)
+    # save_dir = "/home/simjo484/master_thesis/Master_Thesis/classifier_training/" 
+    # with open(save_dir+"t2_training_paths.pkl", "rb") as f:
+    #     train_data_paths = pickle.load(f)
 
-    with open(save_dir+"t2_valid_paths.pkl", "rb") as f:
-        valid_data_paths = pickle.load(f)
+    # with open(save_dir+"t2_valid_paths.pkl", "rb") as f:
+    #     valid_data_paths = pickle.load(f)
+
+    # # Debug mode: Train on very few examples in order to achieve massive speedup, allowing debugging.
+    # if args.debug_mode == True:
+    #     print("\nDebug mode!\n")
+    #     train_data_paths = train_data_paths[0:10]
+    #     valid_data_paths = valid_data_paths[0:10]
+    
+
+    meta_root = "/local/data1/simjo484/mt_data/all_data/MRI/simon/"
+    with open(meta_root+"train_df.pkl", "rb") as f:
+        train_df = pickle.load(f)
+
+    with open(meta_root+"valid_df.pkl", "rb") as f:
+        valid_df = pickle.load(f)
 
     # Debug mode: Train on very few examples in order to achieve massive speedup, allowing debugging.
     if args.debug_mode == True:
         print("\nDebug mode!\n")
-        train_data_paths = train_data_paths[0:10]
-        valid_data_paths = valid_data_paths[0:10]
+        train_df = train_df[0:20]
+        valid_df = valid_df[0:20]
+    
+    # Get loss weights (proportion of each class in training data)
+    n_diags = len(set(train_df["class_label"]))
+    class_counts = collections.Counter(train_df["class_label"]) # A dict with the class counts
+    loss_weights = torch.tensor([1/class_counts[i] for i in range(n_diags)])
+
+    # Format data paths
+    img_root = "/local/data1/simjo484/mt_data/all_data/MRI/pre_processed/Final preprocessed files"
+    train_data_paths = format_paths(train_df, sequences=["T2W", "T2W", "T2W","T2W"], root=img_root)
+    valid_data_paths = format_paths(valid_df, sequences=["T2W", "T2W", "T2W","T2W"], root=img_root)
 
 
     # Define train transform
@@ -63,7 +91,11 @@ def get_loader(args):
         LoadImaged(keys="images"),
         NormalizeIntensityd(keys="images", nonzero=True, channel_wise=True),
         Resized(keys="images", spatial_size=(128, 128, 128)),
-        ToTensord(keys="images", track_meta=False),
+
+        RandRotate90d(keys="images", prob=0.5, max_k=3, spatial_axes=(0,1)),
+        RandFlipd(keys="images", prob=0.5, spatial_axis=2),
+
+        ToTensord(keys="images", track_meta=False)
     ])
 
 
@@ -89,4 +121,32 @@ def get_loader(args):
             valid_ds, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, pin_memory=True, prefetch_factor=4
         )
 
-    return([train_dataloader, valid_dataloader])
+    loss_weights = None
+    return([train_dataloader, valid_dataloader], loss_weights)
+
+
+def format_paths(df, sequences, root, device="cpu"):
+    '''
+    Takes a pd dataframe featuring three columns of T1W, T1W-GD, and T2W image paths and a class label column,
+    and formats the remaining rows in the following manner:
+    
+    [
+    {"images": ["path1", "path2", "path3", "path4"], "label":tensor(0)},
+    ...
+    {"images": ["path1", "path2", "path3", "path4"], "label":tensor(0)}
+    ]
+
+    === Arguments ===
+    df: A pandas dataframe with at least columns T1W, T2W, T1W-GD, class_label.
+
+    sequences: A list specifying the desired combination of sequences, for example
+        ["T2W", "T2W", "T2W", "T2W"] for a T2W only classifier.
+        ["T1W-GD", "T1W-GD", "T2W", "T2W"] for a T1W-GD and T2W classifier.
+    '''
+    paths = []
+    for id, row in df.iterrows():
+        observation = {"images":[root+"/"+row[i] for i in sequences],
+                       "label":torch.tensor(row["class_label"]).to(device).long()}
+        paths.append(observation)
+    
+    return(paths)
